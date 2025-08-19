7168256ae6b8815f92dbcf85c3f6a6f904f5adae
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-08-19 14:14:50 +0600

    done

--- server/serverActions.ts
+++ server/serverActions.ts
@@ -1,8 +1,8 @@
 "use server";
 import { envConfig } from "@/lib/envConfig";
 
-// const BASE_URL = envConfig.SERVER_BASE_URL; // Can be dynamic
-const BASE_URL = envConfig.LOCAL_BASE_URL; // Can be dynamic
+const BASE_URL = envConfig.SERVER_BASE_URL; // Can be dynamic
+// const BASE_URL = envConfig.LOCAL_BASE_URL; // Can be dynamic
 
 // GET ALL DATA
 export const getData = async (endpoint: string, token?: string) => {
